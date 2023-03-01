d3b39cbb48b235b3ffb49e408a53cd2c47855381
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-03-01 11:34:06 +0300

    bitik

--- app/src/main/java/com/example/gelismiskomutlari/MainActivity.java
+++ app/src/main/java/com/example/gelismiskomutlari/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gelismiskomutlari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void uyg1Goster(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg1.class);
+        startActivity(i);
+    }
+
+    public void uyg2Goster(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg2.class);
+        startActivity(i);
+    }
+
+    public void uyg3Goster(View view) {
+        Intent i = new Intent(MainActivity.this, uyg3.class);
+        startActivity(i);
+    }
+
+    public void uyg4Goster(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg4.class);
+        startActivity(i);
     }
 }
